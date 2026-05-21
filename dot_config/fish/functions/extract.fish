6db69d56ee362ca5debec91be7938b93107ac2bf
Rowell Urbaez Reyes <167712855+Row0902@users.noreply.github.com>
2026-05-21 03:16:20 -0400

    fix: improve fish functions - remove eval, fix edit msg, add extract formats
    - multidir: replace dangerous eval with fish -c + pushd/popd
    - edit: show launch message before opening editor
    - extract: add support for tar.zst, tar.xz, .xz, .zst formats
    - cdl: flatten nested if/else, use command ls for safety

--- dot_config/fish/functions/extract.fish
+++ dot_config/fish/functions/extract.fish
@@ -1,32 +1,42 @@
 function extract --description "Expandir o extraer archivos comprimidos automáticamente"
-    if test -f "$argv[1]"
-        switch "$argv[1]"
-            case '*.tar.bz2'
-                tar xjf "$argv[1]"
-            case '*.tar.gz'
-                tar xzf "$argv[1]"
-            case '*.bz2'
-                bunzip2 "$argv[1]"
-            case '*.rar'
-                unrar e "$argv[1]"
-            case '*.gz'
-                gunzip "$argv[1]"
-            case '*.tar'
-                tar xf "$argv[1]"
-            case '*.tbz2'
-                tar xjf "$argv[1]"
-            case '*.tgz'
-                tar xzf "$argv[1]"
-            case '*.zip'
-                unzip "$argv[1]"
-            case '*.Z'
-                uncompress "$argv[1]"
-            case '*.7z'
-                7z x "$argv[1]"
-            case '*'
-                echo "'$argv[1]' no puede ser extraído mediante extract"
-        end
-    else
+    if not test -f "$argv[1]"
         echo "'$argv[1]' no es un archivo válido"
+        return 1
+    end
+
+    switch "$argv[1]"
+        case '*.tar.bz2'
+            tar xjf "$argv[1]"
+        case '*.tar.gz'
+            tar xzf "$argv[1]"
+        case '*.tar.xz'
+            tar xJf "$argv[1]"
+        case '*.tar.zst'
+            tar --zstd -xf "$argv[1]"
+        case '*.bz2'
+            bunzip2 "$argv[1]"
+        case '*.rar'
+            unrar x "$argv[1]"
+        case '*.gz'
+            gunzip "$argv[1]"
+        case '*.tar'
+            tar xf "$argv[1]"
+        case '*.tbz2'
+            tar xjf "$argv[1]"
+        case '*.tgz'
+            tar xzf "$argv[1]"
+        case '*.zip'
+            unzip "$argv[1]"
+        case '*.zst'
+            unzstd "$argv[1]"
+        case '*.xz'
+            unxz "$argv[1]"
+        case '*.7z'
+            7z x "$argv[1]"
+        case '*.Z'
+            uncompress "$argv[1]"
+        case '*'
+            echo "'$argv[1]' no puede ser extraído mediante extract"
+            return 1
     end
 end
