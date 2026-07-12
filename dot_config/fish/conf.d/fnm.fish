f68c6602dd49a3dfeed61f93b87fcf36949339f3
Rowell Urbaez Reyes <167712855+Row0902@users.noreply.github.com>
2026-07-12 09:45:30 -0400

    chore: sync dotfiles

--- dot_config/fish/conf.d/fnm.fish
+++ dot_config/fish/conf.d/fnm.fish
@@ -4,7 +4,11 @@ if test -d "$brew_bin"
     fish_add_path "$brew_bin"
 end
 
-# Usamos --use-on-cd para que cambie de versión según el directorio
 if type -q fnm
-    fnm env --use-on-cd --shell fish | source
+    set -l old_xdg $XDG_RUNTIME_DIR
+    set -e XDG_RUNTIME_DIR
+
+    fnm env --use-on-cd --corepack-enabled --shell fish | source
+
+    set -gx XDG_RUNTIME_DIR $old_xdg
 end
